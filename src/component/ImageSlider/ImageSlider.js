7b75c7f58cffd2c7072086acc145a96eff1a4393
Adminka <nsokolova0399@mail.ru>
2023-04-15 16:23:20 +0300

    Correction of the useEffect error

--- src/component/ImageSlider/ImageSlider.js
+++ src/component/ImageSlider/ImageSlider.js
@@ -4,15 +4,16 @@ import Home from "../../page/Home/Home";
 import Page1 from "../../page/Page1/Page1";
 import Page2 from "../../page/Page2/Page2";
 
-function ImageSlider({ diffx, diffy}) {
-    const [current, setCurrent] = useState(0);
-    let len = 3;
+function ImageSlider({diffx, diffy}) {
+    let [current, setCurrent] = useState(0);
+
     useEffect(()=>{
         if(Math.abs(diffx) > Math.abs(diffy)){
-            if(diffx > 0) setCurrent(current === 0 ? len - 1 : current - 1)
-            else setCurrent(current === len - 1 ? 0 : current + 1)
+            if(diffx > 0) setCurrent(current === 0 ? 3 - 1 : current - 1)
+            else setCurrent(current === 3 - 1 ? 0 : current + 1)
         }
-    }, [diffx, diffy, current, len])
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [diffx, diffy])
 
     return (
         <>
